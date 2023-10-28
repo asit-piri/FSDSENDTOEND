23262b9f1b807b8cc1e8c704ee14d2219a865a9f
Asit Piri <asit.piri@gmail.com>
2023-10-28 09:01:26 +0530

    updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages,setup
 setup(
     name='DiamondPricePrediction',
     version='0.0.1',
-    author='Asit Pirir',
+    author='Asit Piri',
     author_email='asit.piri@gmail.com',
     packages=find_packages()
 )
