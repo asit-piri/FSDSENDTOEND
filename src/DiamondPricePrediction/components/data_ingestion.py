cb85ba3ed2b232a36e8a9dd430d67e8d0b5085c9
Asit Piri <asit.piri@gmail.com>
2023-10-29 11:45:23 +0530

    updated

--- src/DiamondPricePrediction/components/data_ingestion.py
+++ src/DiamondPricePrediction/components/data_ingestion.py
@@ -1,8 +1,7 @@
-
 import pandas as pd
 import numpy as np
-from src.DiamondPricePrediction.logger import logging
-from src.DiamondPricePrediction.exception import customexception
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import customexception
 
 import os
 import sys
@@ -42,6 +41,11 @@ class DataIngestion:
             test_data.to_csv(self.ingestion_config.test_data_path,index=False)
             
             logging.info("data ingestion part completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
             
         except Exception as e:
            logging.info("exception during occured at data ingestion stage")
