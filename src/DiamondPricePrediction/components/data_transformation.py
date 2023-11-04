66e537ef5e7626f1813f2ca085d9c81705b0e80f
Asit Piri <asit.piri@gmail.com>
2023-11-04 08:44:49 +0530

    updated

--- src/DiamondPricePrediction/components/data_transformation.py
+++ src/DiamondPricePrediction/components/data_transformation.py
@@ -18,12 +18,9 @@ from src.DimondPricePrediction.utils.utils import save_object
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
 
-
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
-
-        
+        self.data_transformation_config=DataTransformationConfig()     
     
     def get_data_transformation(self):
         
@@ -67,11 +64,7 @@ class DataTransformation:
             ])
             
             return preprocessor
-            
-
-            
-            
-        
+                                      
         except Exception as e:
             logging.info("Exception occured in the initiate_datatransformation")
 
@@ -96,21 +89,31 @@ class DataTransformation:
             target_feature_train_df=train_df[target_column_name]
             
             
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+            
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
             
             logging.info("Applying preprocessing object on training and testing datasets.")
             
-            
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
+            )
+            
+            logging.info("preprocessing pickle file saved")
+            
+            return (
+                train_arr,
+                test_arr
             )
             
         except Exception as e:
             logging.info("Exception occured in the initiate_datatransformation")
 
             raise customexception(e,sys)
-            
-    
