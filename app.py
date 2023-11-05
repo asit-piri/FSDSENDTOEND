f9cf67970e63aa36e1cd0cc11d44dd7855cc6b04
Asit Piri <asit.piri@gmail.com>
2023-11-05 09:58:20 +0530

    updated

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+# Use this IP to run in web -> http://127.0.0.1:5000
+
 from src.DiamondPricePrediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
 from flask import Flask,request,render_template,jsonify
 
+
 app=Flask(__name__)
+
 
 @app.route('/')
 def home_page():
@@ -38,6 +42,5 @@ def predict_datapoint():
         
         return render_template("result.html",final_result=result)
 
-#execution begin
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run()
